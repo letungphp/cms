0c42e7dae1e62e009be247c45d12bc9c64ce8fd4
letungphp <letungphp@gmail.com>
2018-02-07 11:20:02 +0700

    Debugbar middleware

---
+++ app/Http/Middleware/DebugBarMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class DebugBarMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $admin_id   = 5;
+        $creator_id = 16;
+        $client_id  = 14;
+
+        if(\Auth::check() && in_array(\Auth::user()->id,[$admin_id,$creator_id,$client_id])) {
+            \Debugbar::enable();
+        }else{
+            \Debugbar::disable();
+        }
+        return $next($request);
+    }
+}
